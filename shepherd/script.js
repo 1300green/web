7eb50720d5b98d9e3ddf16b3fccb09b6d0c70dbc
gadmaj <gadymajor@gmail.com>
2026-03-28 19:02:50 -0400

    reworking hebrew

--- shepherd/script.js
+++ shepherd/script.js
@@ -11,7 +11,7 @@ document.getElementById("shepherd").addEventListener("click", function() {
     document.getElementById("shepherd").style.filter = "invert(100%)";
     } 
     if (c >= 13) {
-        window.location.href = "../בָּרָא/";
+        window.location.href = "../cr/";
     }
 
 });
